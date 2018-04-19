2bba454473bef6a00d0fc5b4346addcfdfd8170c
Vibs <vibs2006@gmail.com>
2018-04-19 21:06:57 +0530

    Custom CRM Project Added. It is requested to all contributors please take care of relative folder paths otherwise we might risk in conflict in case of multiple solutions

---
+++ CustomCRM/CustomCRM.Models/CustomCRM.Models/IPerson.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomCRM.Models
+{
+    public interface IPerson<TKey>
+    {
+         TKey ID { get; set; }
+         string Name { get; set; }
+    }
+}
---
+++ CustomCRM/CustomCRM/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CustomCRM.Global" Language="C#" %>
